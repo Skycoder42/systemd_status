be9f30b18abf62608256e63083940c4d1d8d5b57
Skycoder42 <Skycoder42@users.noreply.github.com>
2024-03-14 17:54:07 +0000

    disable web server

--- systemd_status_server/lib/server.dart
+++ systemd_status_server/lib/server.dart
@@ -2,7 +2,6 @@ import 'package:serverpod/serverpod.dart';
 
 import 'src/generated/endpoints.dart';
 import 'src/generated/protocol.dart';
-import 'src/web/routes/root.dart';
 
 // This is the starting point of your Serverpod server. In most cases, you will
 // only need to make additions to this file if you add future calls,  are
@@ -19,14 +18,11 @@ Future<void> run(List<String> args) async {
   // If you are using any future calls, they need to be registered here.
   // pod.registerFutureCall(ExampleFutureCall(), 'exampleFutureCall');
 
-  // Setup a default page at the web root.
-  pod.webServer.addRoute(RouteRoot(), '/');
-  pod.webServer.addRoute(RouteRoot(), '/index.html');
   // Serve all files in the /static directory.
-  pod.webServer.addRoute(
-    RouteStaticDirectory(serverDirectory: 'static', basePath: '/'),
-    '/*',
-  );
+  // pod.webServer.addRoute(
+  //   RouteStaticDirectory(serverDirectory: 'static', basePath: '/'),
+  //   '/*',
+  // );
 
   // Start the server.
   await pod.start();
