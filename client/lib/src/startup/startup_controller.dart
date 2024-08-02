f5a99d89cbf9b82d95a1f712c1907608019b69f4
Skycoder42 <Skycoder42@users.noreply.github.com>
2024-08-02 08:50:33 +0200

    cleanup wasm tests

--- client/lib/src/startup/startup_controller.dart
+++ client/lib/src/startup/startup_controller.dart
@@ -2,5 +2,5 @@ export 'common/startup_controller.dart'
     show clientConfigProvider, serverUrlProvider;
 
 export 'common/startup_controller.dart'
-    if (dart.library.html) 'web/startup_controller.dart'
+    if (dart.library.js_interop) 'web/startup_controller.dart'
     if (dart.library.io) 'vm/startup_controller.dart' show StartupController;
