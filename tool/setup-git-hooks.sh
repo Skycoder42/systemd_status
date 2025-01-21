e26ff8fc4a61df51c00d8efe13c9f7228255e97b
Skycoder42 <Skycoder42@users.noreply.github.com>
2025-01-21 06:14:27 +0000

    use upstream secure storage again

--- tool/setup-git-hooks.sh
+++ tool/setup-git-hooks.sh
@@ -7,10 +7,12 @@ cat << EOF > .git/hooks/pre-commit
 #!/bin/bash
 set -eo pipefail
 
+echo '=== Checking server ==='
 pushd server > /dev/null
 dart run dart_pre_commit
 popd > /dev/null
 
+echo '=== Checking client ==='
 pushd client > /dev/null
 dart run dart_pre_commit
 popd > /dev/null
